c5dfcbb1914361ee96f62cea543d4bd6b5f407f1
sjk1990 <53134582+sjk1990@users.noreply.github.com>
2020-10-02 17:16:48 +0900

    todoList save at localStorage and listup

--- todo/todo.js
+++ todo/todo.js
@@ -2,16 +2,30 @@ const toDoForm = document.querySelector('.js-toDoForm');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('.js-toDoList');
 const TODOS_LS = 'toDos'
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
-    delBtn.innerText = '✔';
     const span = document.createElement('span');
+    const newId = toDos.length + 1;
+    delBtn.innerText = '✔';
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,9 +37,12 @@ function handleSubmit(event) {
 
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+    if(loadedtoDos !== null) {
+        const parsedToDos = JSON.parse(loadedtoDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
