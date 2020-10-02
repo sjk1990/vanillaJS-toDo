01a43722fa30521f5eaa9d9441f66c0ec5c57b45
sjk1990 <53134582+sjk1990@users.noreply.github.com>
2020-10-02 17:36:13 +0900

    delete list and save

--- todo/todo.js
+++ todo/todo.js
@@ -2,7 +2,18 @@ const toDoForm = document.querySelector('.js-toDoForm');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('.js-toDoList');
 const TODOS_LS = 'toDos'
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    })
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,6 +25,7 @@ function paintToDo(text) {
     const span = document.createElement('span');
     const newId = toDos.length + 1;
     delBtn.innerText = '✔';
+    delBtn.addEventListener('click', deleteToDo);
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
