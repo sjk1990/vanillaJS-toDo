af2c46014fb6bfdcde572e93d7a7bd2646a9a392
sjk1990 <53134582+sjk1990@users.noreply.github.com>
2020-10-02 15:51:35 +0900

    practice vanilla js

--- todo/index.js
+++ todo/index.js
@@ -1,1 +1,13 @@
-alert('This Works!') 
+const title = document.querySelector('#title');
+
+const CLICKED = 'clicked';
+
+function handleClick() {
+    title.classList.toggle(CLICKED);
+}
+
+function init() {
+    title.addEventListener('click', handleClick);
+}
+
+init();
